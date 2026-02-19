9cc6270405313e62a8a95b3ec6161abb71aeef1c
Perras2357 <121203082+Perras2357@users.noreply.github.com>
2026-02-20 00:50:36 +0100

    Afficher l'extrait mis en évidence pour le premier résultat de recherche

--- PageRank/main.m
+++ PageRank/main.m
@@ -22,18 +22,32 @@ r = calculerPageRank(G);  % version eig ou power iteration, au choix
 
 % 6) Recherche + tri
 query = input('Mot recherché (vide pour ignorer) : ', 's');
+
 if ~isempty(strtrim(query))
-    ranked = rechercherEtClasserV2(files, contents, r, query);
+    [ranked, matchCount, bestMatchedTokens] = rechercherEtClasserV2(files, contents, r, query);
 
     if isempty(ranked)
         disp("Aucun fichier ne correspond à cette requête.");
     else
-        disp("=== Résultats (triés par nb de mots matchés puis PageRank) ===");
+        qTokens = unique(tokeniserTexte(query));
+        disp("=== Résultats (triés par nb mots matchés puis PageRank) ===");
         for k = 1:numel(ranked)
             i = ranked(k);
+
+            % tokens du doc
+            docTokens = unique(tokeniserTexte(contents{i}));
+
+            % mots de la requête présents dans ce doc
+            present = ismember(qTokens, docTokens);
+            matchedTokensDoc = qTokens(present);
+
             fprintf('%d) %s\n', k, files{i});
+
+            fragmant = extraireFragmant(contents{i}, matchedTokensDoc, 17);
+            disp(fragmant);
+            disp(" "); % ligne vide pour lisibilité
         end
+
     end
 end
-
 
